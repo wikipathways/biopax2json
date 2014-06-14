9712973fd9902ddd447eaf6cd3f95df168bed53b
Anders Riutta <git@andersriutta.com>
2014-06-13 19:05:06 -0700

    now we are getting PublicationXrefs. added tests.

--- biopax.js
+++ biopax.js
@@ -2,16 +2,26 @@
 module.exports = {
   // TODO get ontology terms and other data
 
-  toJson: function(args, callback) {
-    var xmlSelection = args.data;
-    var jsonld = args.jsonld;
-    var xmlBiopaxPubs = xmlSelection.find('PublicationXref');
+  toJson: function($, pathwayMetadata, callback) {
     var jsonBiopax = {};
-    jsonBiopax.publicationXref = [];
-    xmlBiopaxPubs.each(function() {
+    jsonBiopax.xref = [];
+
+    $('bp\\:PublicationXref').each(function() {
+      var xmlPublicationXrefSelection = $( this );
+      console.log('xmlPublicationXrefSelection');
+      console.log(xmlPublicationXrefSelection);
       var publicationXref = {};
-      publicationXref.id = $(this).attr('rdf:ID').toString();
-      jsonBiopax.publicationXref.push(publicationXref);
+      publicationXref.type = 'PublicationXref';
+      publicationXref.id = xmlPublicationXrefSelection.attr('rdf:ID') || xmlPublicationXrefSelection.attr('rdf:about');
+      var dbNameElement = xmlPublicationXrefSelection.find('bp\\:db')[0];
+      publicationXref.dbName = $( dbNameElement ).text().toLowerCase();
+      var dbIdElement = xmlPublicationXrefSelection.find('bp\\:id')[0];
+      publicationXref.dbId = $( dbIdElement ).text();
+      if (publicationXref.id.indexOf('identifiers') === -1 && (publicationXref.dbName.toLowerCase() === 'pubmed' || publicationXref.dbName.toLowerCase() === 'medline') && /^\d+$/g.test(publicationXref.dbId)) {
+        publicationXref.deprecatedId = publicationXref.id;
+        publicationXref.id = 'http://identifiers.org/pubmed/' + publicationXref.dbId;
+      }
+      jsonBiopax.xref.push(publicationXref);
     });
     callback(null, jsonBiopax);
   }
