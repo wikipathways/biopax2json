0340202bb47922768f6dc4c06147635943dc689f
Anders Riutta <git@andersriutta.com>
2014-06-19 22:54:46 -0700

    update to try to make it work in both node and in the browser

--- biopax.js
+++ biopax.js
@@ -1,35 +1,44 @@
-var Cheerio = require('cheerio');
 'use strict';
 module.exports = {
   // TODO get ontology terms and other data
 
   toJson: function(str, pathwayMetadata, callback) {
-    $ = Cheerio.load(str, {
-      normalizeWhitespace: true,
-    xmlMode: true,
-    decodeEntities: true,
-    lowerCaseTags: false
-    });
+
+
+    var xmlSelection;
+    if (typeof window === 'undefined') {
+      var Cheerio = require('cheerio');
+      xmlSelection = Cheerio.load(str, {
+        normalizeWhitespace: true,
+        xmlMode: true,
+        decodeEntities: true,
+        lowerCaseTags: false
+      });
+    } else {
+      var xmlDoc = $.parseXML( str );
+      xmlSelection = $( xmlDoc.documentElement );
+      //xmlSelection = $( xmlDoc );
+    }
 
     var jsonBiopax = pathwayMetadata;
     jsonBiopax.entities = [];
 
-    var displayId = 1;
-    $('bp\\:PublicationXref').each(function() {
-      var xmlPublicationXrefSelection = $( this );
+    var displayName = 1;
+    xmlSelection('bp\\:PublicationXref').each(function() {
+      var xmlPublicationXrefSelection = xmlSelection( this );
       var publicationXref = {};
       publicationXref.type = 'PublicationXref';
       publicationXref.id = xmlPublicationXrefSelection.attr('rdf:ID') || xmlPublicationXrefSelection.attr('rdf:about');
       var dbNameElement = xmlPublicationXrefSelection.find('bp\\:db')[0];
-      publicationXref.dbName = $( dbNameElement ).text().toLowerCase();
+      publicationXref.dbName = xmlSelection( dbNameElement ).text().toLowerCase();
       var dbIdElement = xmlPublicationXrefSelection.find('bp\\:id')[0];
-      publicationXref.dbId = $( dbIdElement ).text();
+      publicationXref.dbId = xmlSelection( dbIdElement ).text();
       if (publicationXref.id.indexOf('identifiers') === -1 && (publicationXref.dbName.toLowerCase() === 'pubmed' || publicationXref.dbName.toLowerCase() === 'medline') && /^\d+$/g.test(publicationXref.dbId)) {
         publicationXref.deprecatedId = publicationXref.id;
         publicationXref.id = 'http://identifiers.org/pubmed/' + publicationXref.dbId;
       }
-      publicationXref.displayId = displayId;
-      displayId += 1;
+      publicationXref.displayName = displayName;
+      displayName += 1;
       jsonBiopax.entities.push(publicationXref);
     });
     callback(null, jsonBiopax);
