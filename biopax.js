f9831ab79d10bf0bd9f62d89125dd4e69468a095
Anders Riutta <git@andersriutta.com>
2014-06-20 11:33:49 -0700

    handle Cheerio/$ for browser/Node better

--- biopax.js
+++ biopax.js
@@ -12,34 +12,46 @@ module.exports = {
     // so to make this work in both Node.js and the browser, I'm removing the namespace prefixes from element tagNames
     // namespace prefixes appear to work OK as attribute names.
     var denamespacedStr = str.replace(/bp\:/g, '');
-    if (typeof window === 'undefined') {
+    var $ = $ || null;
+    if (!$) {
+      // Cheerio allows us to use jQuery selectors in Node.js
+    //if (typeof window === 'undefined') {
       var Cheerio = require('cheerio');
-      xmlSelection = Cheerio.load(denamespacedStr, {
+      $ = Cheerio.load(denamespacedStr, {
         normalizeWhitespace: true,
         xmlMode: true,
         decodeEntities: true,
         lowerCaseTags: false
       });
+      xmlSelection = $.root();
+      //xmlSelection = $('Biopax');
+      console.log(xmlSelection);
     } else {
+
+
+      var xmlDoc = $.parseXML(str);
+      xmlSelection = $(xmlDoc);
+
+      /*
       var xmlDoc = $.parseXML( str );
       xmlSelection = $( xmlDoc );
       //xmlSelection = $( xmlDoc );
+      //*/
     }
 
     var jsonBiopax = pathwayMetadata;
     jsonBiopax.entities = [];
 
     var displayName = 1;
-    xmlSelection('PublicationXref').each(function() {
-      var xmlPublicationXrefSelection = xmlSelection( this );
+    xmlSelection.find('PublicationXref').each(function() {
+      var xmlPublicationXrefSelection = $( this );
       var publicationXref = {};
       publicationXref.type = 'PublicationXref';
       publicationXref.id = xmlPublicationXrefSelection.attr('rdf:ID') || xmlPublicationXrefSelection.attr('rdf:about');
-      var dbNameElement = xmlPublicationXrefSelection.find('db')[0];
-      publicationXref.dbName = xmlSelection( dbNameElement ).text().toLowerCase();
-      var dbIdElement = xmlPublicationXrefSelection.find('id')[0];
-      publicationXref.dbId = xmlSelection( dbIdElement ).text();
-      if (publicationXref.id.indexOf('identifiers') === -1 && (publicationXref.dbName.toLowerCase() === 'pubmed' || publicationXref.dbName.toLowerCase() === 'medline') && /^\d+$/g.test(publicationXref.dbId)) {
+      publicationXref.dbName = xmlPublicationXrefSelection.find('db').text().toLowerCase();
+      publicationXref.dbId = xmlPublicationXrefSelection.find('id').text();
+      // convert the id to an identifiers.org URI, if it's a pubmed reference and not already an identifiers.org URI
+      if (publicationXref.id.indexOf('identifiers') === -1 && (publicationXref.dbName === 'pubmed' || publicationXref.dbName === 'medline') && /^\d+$/g.test(publicationXref.dbId)) {
         publicationXref.deprecatedId = publicationXref.id;
         publicationXref.id = 'http://identifiers.org/pubmed/' + publicationXref.dbId;
       }
